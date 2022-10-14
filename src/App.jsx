bd2a56aba49082ef4b74c9c742fb9d09b020872b
Rupali Anjaria-Rami <rupalianjaria-rami@Rupalis-iMac.home>
2022-10-14 12:43:01 -0400

    Nav and Button Components completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Nav from './components/Nav/Nav';
+import Button from './components/Button/Button';
+
+const App = () => {
+
+  return (
+    <div className="app">
+      <Nav />
+      <p className='footer'>
+        Nothing to see here yet...Add a task in the field above!
+      </p>
+        
+    </div>
+  );
+}
+
+export default App;
